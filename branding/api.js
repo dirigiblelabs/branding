ecc1fcdf209816141a2a23cf18baa8623a499350
ThuF <jordan.p.pavlov@gmail.com>
2018-09-21 10:18:36 +0300

    Adds Branding Icon

--- branding/api.js
+++ branding/api.js
@@ -2,13 +2,17 @@ var rs = require('http/v3/rs');
 var configurations = require('core/v3/configurations');
 
 var DIRIGIBLE_BRANDING_NAME = 'DIRIGIBLE_BRANDING_NAME';
+var DIRIGIBLE_BRANDING_ICON = 'DIRIGIBLE_BRANDING_ICON';
+
 var DIRIGIBLE_BRANDING_NAME_DEFAULT = 'Eclipse Dirigible';
+var DIRIGIBLE_BRANDING_ICON_DEFAULT = '../../../../services/v3/web/resources/images/favicon.png';
 
 rs.service()
 	.resource('')
 		.get(function(ctx, request, response) {
 			var branding = {
-				'name': configurations.get(DIRIGIBLE_BRANDING_NAME, DIRIGIBLE_BRANDING_NAME_DEFAULT)
+				'name': configurations.get(DIRIGIBLE_BRANDING_NAME, DIRIGIBLE_BRANDING_NAME_DEFAULT),
+				'icon': configurations.get(DIRIGIBLE_BRANDING_ICON, DIRIGIBLE_BRANDING_ICON_DEFAULT)
 			};
             response.println(JSON.stringify(branding));
 		})
