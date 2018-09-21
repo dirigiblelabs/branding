100fefff2431dc14012e411177a87f47b9117eba
ThuF <jordan.p.pavlov@gmail.com>
2018-09-21 10:37:57 +0300

    Adds Brand Property

--- branding/api.js
+++ branding/api.js
@@ -2,9 +2,11 @@ var rs = require('http/v3/rs');
 var configurations = require('core/v3/configurations');
 
 var DIRIGIBLE_BRANDING_NAME = 'DIRIGIBLE_BRANDING_NAME';
+var DIRIGIBLE_BRANDING_BRAND = 'DIRIGIBLE_BRANDING_BRAND';
 var DIRIGIBLE_BRANDING_ICON = 'DIRIGIBLE_BRANDING_ICON';
 
 var DIRIGIBLE_BRANDING_NAME_DEFAULT = 'Eclipse Dirigible';
+var DIRIGIBLE_BRANDING_BRAND_DEFAULT = 'Eclipse Dirigible';
 var DIRIGIBLE_BRANDING_ICON_DEFAULT = '../../../../services/v3/web/resources/images/favicon.png';
 
 rs.service()
@@ -12,6 +14,7 @@ rs.service()
 		.get(function(ctx, request, response) {
 			var branding = {
 				'name': configurations.get(DIRIGIBLE_BRANDING_NAME, DIRIGIBLE_BRANDING_NAME_DEFAULT),
+				'brand': configurations.get(DIRIGIBLE_BRANDING_BRAND, DIRIGIBLE_BRANDING_BRAND_DEFAULT),
 				'icon': configurations.get(DIRIGIBLE_BRANDING_ICON, DIRIGIBLE_BRANDING_ICON_DEFAULT)
 			};
             response.println(JSON.stringify(branding));
